eddd7505575e9f89b0232ba869818978e3ab96bd
Luis Cruz Liñan <luiscruzlinan@outloo.com>
2020-05-09 00:05:08 -0500

    clase del 8 de mayo

---
+++ Ejercicio_4.py
@@ -0,0 +1,38 @@
+Hmayor=0
+Hmenor=0
+Mmayor=0
+Mmenor=0
+suma=0
+res=input("Regitrar Persona?: ")
+while(res!="no"):
+    sexo=input("Ingresa tu Sexo: ")
+    if(sexo=="H" or sexo=="F"):
+        edad=int(input("Ingresa tu Edad: "))
+        if(sexo=="H"):
+            if(edad>=18):
+                Hmayor+=1
+            else:
+                Hmenor+=1
+        else:
+            if(edad>=18):
+                Mmayor+=1
+            else:
+                Mmenor+=1
+        if(Hmayor==5):
+            print("Se han Censado 5 Hombres Mayores de Edad")
+        elif(Hmenor==5):
+            print("Se han Censado 5 Hombres Menores de Edad")
+        if(Mmayor==5):
+            print("Se han Censado 5 Mujeres Mayores de Edad")
+        elif(Mmenor==5):
+            print("Se han Censado 5 Mujeres Menores de Edad")
+        suma+=1
+    else:
+        print("Sexo Erroneo")
+        break
+    res=input("Registro Persona?: ")
+print("Numero de Personas Censadas: ",suma)
+print("Hombres Mayores de edad: ",Hmayor)
+print("Hombres Menores de edad: ",Hmenor)
+print("Mujeres Mayores de edad: ",Mmayor)
+print("Mujeres Menores de edad: ",Mmenor)
